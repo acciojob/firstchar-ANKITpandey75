f20611700e2eeef2c750a4f74c84824d06ce6e37
ANKITpandey75 <125044396+ANKITpandey75@users.noreply.github.com>
2026-06-25 03:39:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function firstChar(text) {
-  text = text.trim();
-  return text.length > 0 ? text[0] : "";
+  return text.trim().charAt(0);
 }
 
 // Do not change the code below
-// Uncomment the following line to show the prompt popup
-// const text = prompt("Enter text:");
+//Uncomment the following line to show the prompt popup
+//const text = prompt("Enter text:");
 alert(firstChar(text));
